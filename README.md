a9276c41670e45437fecf47f35ce3412094cdac7
Guillaume Molle <heryu@hotmail.fr>
2023-12-16 20:18:37 +0100

    feat: add Game & Watch Gallery 3 (GBC) [save]

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 | Games                                     | Console | Editor     | Region                      |
 | ----------------------------------------- | ------- | ---------- | --------------------------- |
 | Castlevania: Circle of the Moon           | GBA     | Save / Rom | EUR / USA / JAP             |
+| Game & Watch Gallery 3                    | GBC     | Save       | EUR / USA / JAP / AUS       |
 | Konami Krazy Racers                       | GBA     | Save       | EUR / USA / JAP             |
 | Legend of Zelda, The: Link's Awakening DX | GBC     | Save       | EUR / USA / JAP / FRA / GER |
 | Mystic Quest Legend                       | SNES    | Save / Rom | EUR / USA / JAP / FRA / GER |
---
+++ src/lib/templates/game-and-watch-gallery-3-gbc/saveEditor/utils.ts
@@ -0,0 +1,66 @@
+import { getInt, setInt } from "$lib/utils/bytes";
+
+import type { Item, ItemChecksum, ItemInt } from "$lib/types";
+
+export function overrideGetInt(
+  item: Item,
+): [boolean, number | string | undefined] {
+  if ("id" in item && item.id?.match(/score-/)) {
+    const itemInt = item as ItemInt;
+
+    const digit1 = getInt(itemInt.offset, "uint8");
+    const digit2 = getInt(itemInt.offset + 1, "uint8");
+    const digit3 = getInt(itemInt.offset + 2, "uint8");
+    const digit4 = getInt(itemInt.offset + 3, "uint8");
+
+    let int = 0;
+
+    if (item.id === "score-2") {
+      int = digit1 * 10 + digit2;
+    } else if (item.id === "score-4") {
+      int = digit1 * 1000 + digit2 * 100 + digit3 * 10 + digit4;
+    }
+
+    return [true, int];
+  }
+
+  return [false, undefined];
+}
+
+export function overrideSetInt(item: Item, value: string): boolean {
+  if ("id" in item && item.id?.match(/score-/)) {
+    const itemInt = item as ItemInt;
+
+    let array: string[] = [];
+
+    if (item.id === "score-2") {
+      array = value.padStart(2, "0").split("");
+    } else if (item.id === "score-4") {
+      array = value.padStart(3, "0").split("");
+    }
+
+    array.forEach((digit, index) =>
+      setInt(itemInt.offset + index, "uint8", digit),
+    );
+
+    return true;
+  }
+
+  return false;
+}
+
+export function generateChecksum(item: ItemChecksum): number {
+  let checksum = 0x0;
+
+  for (
+    let i = item.control.offset;
+    i < item.control.offset + item.control.length;
+    i += 0x1
+  ) {
+    if (i < item.offset) {
+      checksum += getInt(i, "uint8");
+    }
+  }
+
+  return checksum;
+}
