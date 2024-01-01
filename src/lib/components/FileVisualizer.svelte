fcee7076020287ab31a92e30858a68f464f049d9
Guillaume Molle <heryu@hotmail.fr>
2024-01-01 01:55:34 +0100

    feat: virtualize FileVisualizer

--- src/lib/components/FileVisualizer.svelte
+++ src/lib/components/FileVisualizer.svelte
@@ -1,20 +1,30 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+  import { onDestroy, onMount } from "svelte";
 
   import { dataView, gameJson, isFileVisualizerOpen } from "$lib/stores";
-  import {
-    addItem,
-    parseItem,
-    parseValidator,
-  } from "$lib/utils/fileVisualizer";
+  import { parseItem, parseValidator } from "$lib/utils/fileVisualizer";
 
   import type { HighlightedOffsets } from "$lib/utils/fileVisualizer";
 
+  const bodyEl = document.querySelector("body") as HTMLBodyElement;
+
+  let contentEl: HTMLDivElement;
   let tooltipEl: HTMLDivElement;
 
-  let dataLength = 0;
+  let contentHeight = 0;
+  let visibleRows: number[] = [];
+
+  let paddingTop = 0;
+  let paddingBottom = 0;
+
+  let start = 0;
+  let end = 0;
+
   let highlightedOffsets: HighlightedOffsets = {};
   let tooltip = "";
+
+  const rows = [...Array(Math.floor($dataView.byteLength / 0x10)).keys()];
+  const rowHeight = 24;
 
   function handleClose(): void {
     $isFileVisualizerOpen = false;
@@ -41,8 +51,40 @@
     tooltip = "";
   }
 
+  function handleScroll() {
+    const { scrollTop } = contentEl;
+
+    let i = 0;
+    let y = 0;
+
+    while (i < rows.length) {
+      if (y + rowHeight > scrollTop) {
+        start = i;
+        paddingTop = y;
+
+        break;
+      }
+
+      y += rowHeight;
+      i += 1;
+    }
+
+    while (i < rows.length) {
+      y += rowHeight;
+      i += 1;
+
+      if (y > scrollTop + contentHeight) {
+        break;
+      }
+    }
+
+    end = i;
+
+    paddingBottom = (rows.length - end) * rowHeight;
+  }
+
   onMount(() => {
-    dataLength = $dataView.byteLength;
+    bodyEl.style.overflow = "hidden";
 
     parseValidator(highlightedOffsets);
 
@@ -50,34 +92,61 @@
       parseItem(highlightedOffsets, item);
     });
   });
+
+  onDestroy(() => {
+    bodyEl.style.overflow = "auto";
+  });
+
+  $: {
+    if (contentEl) {
+      handleScroll();
+
+      visibleRows = rows.slice(start, end).map((i) => i * 0x10);
+    }
+  }
 </script>
 
 <div class="gtc-filevisualizer-backdrop" on:click={handleClose}>
-  <div
-    class="gtc-filevisualizer"
-    on:blur={handleMouseOut}
-    on:click|stopPropagation
-    on:mouseout={handleMouseOut}
-  >
-    <div class="gtc-filevisualizer-offsets">
-      {#each [...Array(Math.floor(dataLength / 0x10) + 1).keys()] as offset}
-        <div class="gtc-filevisualizer-hex">{(offset * 0x10).toHex(8)}</div>
-      {/each}
-    </div>
-    <div class="gtc-filevisualizer-bytes">
-      {#each [...Array(dataLength).keys()] as offset}
-        <div
-          class="gtc-filevisualizer-hex{highlightedOffsets[offset] !==
-            undefined && highlightedOffsets[offset].dataType
-            ? ` gtc-filevisualizer-hex-${highlightedOffsets[offset].dataType}`
-            : ''}"
-          class:gtc-filevisualizer-hex-bitflags={highlightedOffsets[offset]
-            ?.type === "bitflags"}
-          on:mousemove={(event) => handleMouseMove(event, offset)}
-        >
-          {$dataView.getUint8(offset).toHex(2)}
-        </div>
-      {/each}
+  <div class="gtc-filevisualizer">
+    <div
+      class="gtc-filevisualizer-content"
+      bind:this={contentEl}
+      bind:offsetHeight={contentHeight}
+      on:blur={handleMouseOut}
+      on:click|stopPropagation
+      on:mouseout={handleMouseOut}
+      on:scroll={handleScroll}
+    >
+      <div
+        style="padding-top: {paddingTop}px; padding-bottom: {paddingBottom}px;"
+      >
+        {#each visibleRows as row}
+          <div class="gtc-filevisualizer-row">
+            <div class="gtc-filevisualizer-offsets">
+              {row.toHex(8)}
+            </div>
+            <div class="gtc-filevisualizer-bytes">
+              {#each [...Array(0x10).keys()] as offset}
+                <div
+                  class="gtc-filevisualizer-hex{highlightedOffsets[
+                    row + offset
+                  ] !== undefined && highlightedOffsets[row + offset].dataType
+                    ? ` gtc-filevisualizer-hex-${
+                        highlightedOffsets[row + offset].dataType
+                      }`
+                    : ''}"
+                  class:gtc-filevisualizer-hex-bitflags={highlightedOffsets[
+                    row + offset
+                  ]?.type === "bitflags"}
+                  on:mousemove={(event) => handleMouseMove(event, row + offset)}
+                >
+                  {$dataView.getUint8(row + offset).toHex(2)}
+                </div>
+              {/each}
+            </div>
+          </div>
+        {/each}
+      </div>
     </div>
     <div
       class="gtc-filevisualizer-tooltip"
@@ -96,72 +165,77 @@
     z-index: 10000;
 
     & .gtc-filevisualizer {
-      @apply flex justify-center p-4 bg-primary-900 rounded-xl overflow-auto;
+      @apply flex justify-center p-4 h-full bg-primary-900 rounded-xl;
 
       width: 602px;
-      height: 100%;
 
-      & .gtc-filevisualizer-offsets {
-        & .gtc-filevisualizer-hex {
-          @apply px-4 bg-primary-800;
+      & .gtc-filevisualizer-content {
+        @apply overflow-y-auto;
+
+        & .gtc-filevisualizer-row {
+          @apply flex;
+
+          & .gtc-filevisualizer-offsets {
+            @apply px-4 h-6 font-source text-center uppercase bg-primary-800;
+          }
+
+          & .gtc-filevisualizer-bytes {
+            @apply flex content-start flex-wrap px-2;
+
+            width: 452px;
+
+            & .gtc-filevisualizer-hex {
+              @apply px-1 h-6;
+
+              &.gtc-filevisualizer-hex-bitflags {
+                @apply bg-green-700;
+              }
+
+              &.gtc-filevisualizer-hex-bit {
+                @apply bg-emerald-700;
+              }
+
+              &.gtc-filevisualizer-hex-boolean {
+                @apply bg-lime-700;
+              }
+
+              &.gtc-filevisualizer-hex-lower4,
+              &.gtc-filevisualizer-hex-upper4,
+              &.gtc-filevisualizer-hex-int8,
+              &.gtc-filevisualizer-hex-uint8 {
+                @apply bg-cyan-700;
+              }
+
+              &.gtc-filevisualizer-hex-int16,
+              &.gtc-filevisualizer-hex-uint16 {
+                @apply bg-yellow-700;
+              }
+
+              &.gtc-filevisualizer-hex-int24,
+              &.gtc-filevisualizer-hex-uint24 {
+                @apply bg-orange-700;
+              }
+
+              &.gtc-filevisualizer-hex-int32,
+              &.gtc-filevisualizer-hex-uint32 {
+                @apply bg-red-700;
+              }
+
+              &.gtc-filevisualizer-hex-int64,
+              &.gtc-filevisualizer-hex-uint64 {
+                @apply bg-amber-700;
+              }
+
+              &.gtc-filevisualizer-hex-string {
+                @apply bg-indigo-700;
+              }
+            }
+          }
+
+          & .gtc-filevisualizer-hex {
+            @apply font-source text-center uppercase;
+          }
         }
-      }
-
-      & .gtc-filevisualizer-bytes {
-        @apply flex content-start flex-wrap px-2;
-
-        width: 452px;
-
-        & .gtc-filevisualizer-hex {
-          @apply px-1 h-6;
-
-          &.gtc-filevisualizer-hex-bitflags {
-            @apply bg-green-700;
-          }
-
-          &.gtc-filevisualizer-hex-bit {
-            @apply bg-emerald-700;
-          }
-
-          &.gtc-filevisualizer-hex-boolean {
-            @apply bg-lime-700;
-          }
-
-          &.gtc-filevisualizer-hex-lower4,
-          &.gtc-filevisualizer-hex-upper4,
-          &.gtc-filevisualizer-hex-int8,
-          &.gtc-filevisualizer-hex-uint8 {
-            @apply bg-cyan-700;
-          }
-
-          &.gtc-filevisualizer-hex-int16,
-          &.gtc-filevisualizer-hex-uint16 {
-            @apply bg-yellow-700;
-          }
-
-          &.gtc-filevisualizer-hex-int24,
-          &.gtc-filevisualizer-hex-uint24 {
-            @apply bg-orange-700;
-          }
-
-          &.gtc-filevisualizer-hex-int32,
-          &.gtc-filevisualizer-hex-uint32 {
-            @apply bg-red-700;
-          }
-
-          &.gtc-filevisualizer-hex-int64,
-          &.gtc-filevisualizer-hex-uint64 {
-            @apply bg-amber-700;
-          }
-
-          &.gtc-filevisualizer-hex-string {
-            @apply bg-indigo-700;
-          }
-        }
-      }
-
-      & .gtc-filevisualizer-hex {
-        @apply font-source text-center uppercase;
       }
 
       & .gtc-filevisualizer-tooltip {
