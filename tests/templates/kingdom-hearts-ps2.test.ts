edc5190c87cd5bec02a0276e9bb1ffb34991895c
Guillaume Molle <heryu@hotmail.fr>
2024-03-17 18:57:44 +0100

    style: adjust spaces on tests

--- tests/templates/kingdom-hearts-ps2.test.ts
+++ tests/templates/kingdom-hearts-ps2.test.ts
@@ -21,16 +21,16 @@ describe(game, () => {
 
   // prettier-ignore
   test.each([
-    ["should load a filled standard save (Japan)"             ,          "filled.ps2", ["r|japan"    , 't|["Slot 98"]'           , "s|1$1", "i|29$1", "i|39$2" , "s|6$2", "s|2$3", "i|PASS"]],
-    ["should load a filled standard save (Europe, Australia)" ,          "filled.ps2", ["r|australia", 't|["Slot 76"]'           , "s|1$1", "i|31$1", "i|55$2" , "s|6$2", "s|2$3", "i|PASS"]],
-    ["should load a deleted standard save (Slot 13)"          ,  "deleted-slot13.ps2", [               't|["Slot 13"]'           , "s|1$1", "i|30$1", "i|45$2" , "s|6$2", "s|2$3", "i|PASS"]],
-    ["should load a standard save (Europe, Australia)"        , "europeaustralia.ps2", [               't|["Slot 1","Slot 13"]'  , "s|2$1", "i|30$1", "i|45$2" , "s|6$2", "s|2$3", "i|PASS"]],
-    ["should load a standard save (USA)"                      ,             "usa.ps2", [               't|["Slot 37"]'           , "s|1$1", "i|35$1", "i|37$2" , "s|6$2", "s|2$3", "i|PASS"]],
-    ["should load a standard save (Japan)"                    ,           "japan.ps2", [               't|["Slot 1"]'            , "s|1$1", "i|27$1", "i|10$2" , "s|6$2", "s|2$3", "i|PASS"]],
-    ["should load a standard save (France)"                   ,          "france.ps2", [               't|["Slot 94","Slot 97"]' , "s|1$1", "i|30$1", "i|35$2" , "s|6$2", "s|2$3", "i|PASS"]],
-    ["should load a standard save (Germany)"                  ,         "germany.ps2", [               't|["Slot 21"]'           , "s|1$1", "i|33$1", "i|30$2" , "s|6$2", "s|2$3", "i|PASS"]],
-    ["should load a standard save (Italy)"                    ,           "italy.ps2", [               't|["Slot 3","Slot 5"]'   , "s|1$1", "i|33$1", "i|22$2" , "s|6$2", "s|2$3", "i|PASS"]],
-    ["should load a standard save (Spain)"                    ,           "spain.ps2", [               't|["Slot 1"]'            , "s|1$1", "i|30$1", "i|45$2" , "s|6$2", "s|2$3", "i|PASS"]],
+    ["should load a filled standard save (Japan)"            ,          "filled.ps2", ["r|japan"    , 't|["Slot 98"]'           , "s|1$1", "i|29$1", "i|39$2" , "s|6$2", "s|2$3", "i|PASS"]],
+    ["should load a filled standard save (Europe, Australia)",          "filled.ps2", ["r|australia", 't|["Slot 76"]'           , "s|1$1", "i|31$1", "i|55$2" , "s|6$2", "s|2$3", "i|PASS"]],
+    ["should load a deleted standard save (Slot 13)"         ,  "deleted-slot13.ps2", [               't|["Slot 13"]'           , "s|1$1", "i|30$1", "i|45$2" , "s|6$2", "s|2$3", "i|PASS"]],
+    ["should load a standard save (Europe, Australia)"       , "europeaustralia.ps2", [               't|["Slot 1","Slot 13"]'  , "s|2$1", "i|30$1", "i|45$2" , "s|6$2", "s|2$3", "i|PASS"]],
+    ["should load a standard save (USA)"                     ,             "usa.ps2", [               't|["Slot 37"]'           , "s|1$1", "i|35$1", "i|37$2" , "s|6$2", "s|2$3", "i|PASS"]],
+    ["should load a standard save (Japan)"                   ,           "japan.ps2", [               't|["Slot 1"]'            , "s|1$1", "i|27$1", "i|10$2" , "s|6$2", "s|2$3", "i|PASS"]],
+    ["should load a standard save (France)"                  ,          "france.ps2", [               't|["Slot 94","Slot 97"]' , "s|1$1", "i|30$1", "i|35$2" , "s|6$2", "s|2$3", "i|PASS"]],
+    ["should load a standard save (Germany)"                 ,         "germany.ps2", [               't|["Slot 21"]'           , "s|1$1", "i|33$1", "i|30$2" , "s|6$2", "s|2$3", "i|PASS"]],
+    ["should load a standard save (Italy)"                   ,           "italy.ps2", [               't|["Slot 3","Slot 5"]'   , "s|1$1", "i|33$1", "i|22$2" , "s|6$2", "s|2$3", "i|PASS"]],
+    ["should load a standard save (Spain)"                   ,           "spain.ps2", [               't|["Slot 1"]'            , "s|1$1", "i|30$1", "i|45$2" , "s|6$2", "s|2$3", "i|PASS"]],
   ])("%s", async (...args) =>
     await snippet(`${game}/${args[1]}`, args[2]),
   );
--- tests/templates/marvel-vs-capcom-2-new-age-of-heroes-dc.test.ts
+++ tests/templates/marvel-vs-capcom-2-new-age-of-heroes-dc.test.ts
@@ -17,14 +17,14 @@ describe(game, () => {
 
   // prettier-ignore
   test.each([
-    ["should load a filled standard save (Europe)"      , "filled.bin", ["r|europe", "c|2f7a5b30$1", "c|b3a3$2", "i|49$1", "i|20$2" , "w|50$1", "c|2f7b5c30$1", "c|9859$2"]],
-    ["should load a filled standard save (Japan)"       , "filled.bin", ["r|japan" , "c|2fc34f85$1", "c|e474$2", "i|31$1", "i|100$2", "w|32$1", "c|2fc45085$1", "c|9e5d$2"]],
-    ["should load a standard save (Europe)"             , "europe.bin", [            "c|2f6e730c$1", "c|1738$2", "i|31$1", "i|10$2" , "w|32$1", "c|2f6f740c$1", "c|2d75$2"]],
-    ["should load a standard save (USA)"                ,    "usa.bin", [            "c|2fcf6977$1", "c|3cdb$2", "i|11$1", "i|100$2", "w|12$1", "c|2fd06a77$1", "c|d887$2"]],
-    ["should load a standard save (Japan)"              ,  "japan.bin", [            "c|2f74671e$1", "c|1fe0$2", "i|16$1", "i|20$2" , "w|17$1", "c|2f75681e$1", "c|8436$2"]],
-    ["should load a DCI save (Europe)"                  , "europe.dci", [            "c|2fdf737d$1", "c|8555$2", "i|50$1", "i|100$2", "w|51$1", "c|2fe0747d$1", "c|91b8$2"]],
-    ["should load a DCI save (USA)"                     ,    "usa.dci", [            "c|2f866136$1", "c|0fb0$2", "i|39$1", "i|10$2" , "w|40$1", "c|2f876236$1", "c|ada4$2"]],
-    ["should load a DCI save (Japan)"                   ,  "japan.dci", [            "c|2fa74276$1", "c|6b1a$2", "i|00$1", "i|100$2", "w|01$1", "c|2fa84376$1", "c|c8c2$2"]],
+    ["should load a filled standard save (Europe)", "filled.bin", ["r|europe", "c|2f7a5b30$1", "c|b3a3$2", "i|49$1", "i|20$2" , "w|50$1", "c|2f7b5c30$1", "c|9859$2"]],
+    ["should load a filled standard save (Japan)" , "filled.bin", ["r|japan" , "c|2fc34f85$1", "c|e474$2", "i|31$1", "i|100$2", "w|32$1", "c|2fc45085$1", "c|9e5d$2"]],
+    ["should load a standard save (Europe)"       , "europe.bin", [            "c|2f6e730c$1", "c|1738$2", "i|31$1", "i|10$2" , "w|32$1", "c|2f6f740c$1", "c|2d75$2"]],
+    ["should load a standard save (USA)"          ,    "usa.bin", [            "c|2fcf6977$1", "c|3cdb$2", "i|11$1", "i|100$2", "w|12$1", "c|2fd06a77$1", "c|d887$2"]],
+    ["should load a standard save (Japan)"        ,  "japan.bin", [            "c|2f74671e$1", "c|1fe0$2", "i|16$1", "i|20$2" , "w|17$1", "c|2f75681e$1", "c|8436$2"]],
+    ["should load a DCI save (Europe)"            , "europe.dci", [            "c|2fdf737d$1", "c|8555$2", "i|50$1", "i|100$2", "w|51$1", "c|2fe0747d$1", "c|91b8$2"]],
+    ["should load a DCI save (USA)"               ,    "usa.dci", [            "c|2f866136$1", "c|0fb0$2", "i|39$1", "i|10$2" , "w|40$1", "c|2f876236$1", "c|ada4$2"]],
+    ["should load a DCI save (Japan)"             ,  "japan.dci", [            "c|2fa74276$1", "c|6b1a$2", "i|00$1", "i|100$2", "w|01$1", "c|2fa84376$1", "c|c8c2$2"]],
   ])("%s", async (...args) =>
     await snippet(`${game}/${args[1]}`, args[2]),
   );
