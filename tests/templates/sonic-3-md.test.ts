6b17b3cd1bcfb389b07c6f2d6f02454fbadd5881
Guillaume Molle <heryu@hotmail.fr>
2024-03-17 19:43:27 +0100

    fix: tests of Sonic 3 (MD) [save]

--- tests/templates/sonic-3-md.test.ts
+++ tests/templates/sonic-3-md.test.ts
@@ -11,15 +11,15 @@ describe(game, () => {
 
   // prettier-ignore
   test.each([
-    ["should load an empty standard save"                ,           "empty.sav", ['t|["Sonic 3","Sonic 3 & Knuckles","Competition"]$1', 't|[]$2']],
-    ["should load a deleted standard save"               ,         "deleted.sav", ['t|["Sonic 3","Sonic 3 & Knuckles","Competition"]$1', 't|[]$2']],
-    ["should load a standard save (Europe)"              ,          "europe.sav", ['t|["Sonic 3","Sonic 3 & Knuckles","Competition"]$1', 't|["Slot 6"]$2', "c|f0f9", "i|Sonic & Tails$1", "i|Hydrocity Zone$2"   , "w|2$2", "c|c3ff"]],
-    ["should load a standard save (USA)"                 ,             "usa.sav", ['t|["Sonic 3","Sonic 3 & Knuckles","Competition"]$1', 't|["Slot 4"]$2', "c|c7fb", "i|Tails$1"        , "i|Angel Island Zone$2", "w|1$2", "c|81c2"]],
-    ["should load a standard save (Japan)"               ,           "japan.sav", ['t|["Sonic 3","Sonic 3 & Knuckles","Competition"]$1', 't|["Slot 5"]$2', "c|4afe", "i|Sonic$1"        , "i|IceCap Zone$2"      , "w|6$2", "c|2fc2"]],
-    ["should load a standard save (Europe) (Knuckles)"   , "europe-knuckles.sav", ['t|["Sonic 3 & Knuckles","Competition"]$1'          , 't|["Slot 3"]$2', "c|8ed1", "i|Sonic$1"        , "i|Angel Island Zone$2", "w|1$2", "c|1a50"]],
-    ["should load a standard save (USA) (Knuckles)"      ,    "usa-knuckles.sav", ['t|["Sonic 3 & Knuckles","Competition"]$1'          , 't|["Slot 2"]$2', "c|1fbd", "i|Knuckles$1"     , "i|Angel Island Zone$2", "w|1$2", "c|1398"]],
-    ["should load a standard save (Japan) (Knuckles)"    ,  "japan-knuckles.sav", ['t|["Sonic 3 & Knuckles","Competition"]$1'          , 't|["Slot 8"]$2', "c|73f4", "i|Tails$1"        , "i|Angel Island Zone$2", "w|1$2", "c|7b75"]],
-    ["should load a standard save (Japan) (Both)"        ,            "both.sav", ['t|["Sonic 3","Sonic 3 & Knuckles","Competition"]$1', 't|["Slot 4"]$2', "c|c7fb", "i|Tails$1"        , "i|Angel Island Zone$2", "w|1$2", "c|81c2"]],
+    ["should load an empty standard save"             ,           "empty.sav", ['t|["Sonic 3","Sonic 3 & Knuckles","Competition"]$1', 't|[]$2']],
+    ["should load a deleted standard save"            ,         "deleted.sav", ['t|["Sonic 3","Sonic 3 & Knuckles","Competition"]$1', 't|[]$2']],
+    ["should load a standard save (Europe)"           ,          "europe.sav", ['t|["Sonic 3","Sonic 3 & Knuckles","Competition"]$1', 't|["Slot 6"]$2', "c|f0f9", "i|Sonic & Tails$1", "i|1$2", "w|2$2", "c|c3ff"]],
+    ["should load a standard save (USA)"              ,             "usa.sav", ['t|["Sonic 3","Sonic 3 & Knuckles","Competition"]$1', 't|["Slot 4"]$2', "c|c7fb", "i|Tails$1"        , "i|0$2", "w|1$2", "c|81c2"]],
+    ["should load a standard save (Japan)"            ,           "japan.sav", ['t|["Sonic 3","Sonic 3 & Knuckles","Competition"]$1', 't|["Slot 5"]$2', "c|4afe", "i|Sonic$1"        , "i|5$2", "w|6$2", "c|2fc2"]],
+    ["should load a standard save (Europe) (Knuckles)", "europe-knuckles.sav", ['t|["Sonic 3 & Knuckles","Competition"]$1'          , 't|["Slot 3"]$2', "c|8ed1", "i|Sonic$1"        , "i|0$2", "w|1$2", "c|1a50"]],
+    ["should load a standard save (USA) (Knuckles)"   ,    "usa-knuckles.sav", ['t|["Sonic 3 & Knuckles","Competition"]$1'          , 't|["Slot 2"]$2', "c|1fbd", "i|Knuckles$1"     , "i|0$2", "w|1$2", "c|1398"]],
+    ["should load a standard save (Japan) (Knuckles)" ,  "japan-knuckles.sav", ['t|["Sonic 3 & Knuckles","Competition"]$1'          , 't|["Slot 8"]$2', "c|73f4", "i|Tails$1"        , "i|0$2", "w|1$2", "c|7b75"]],
+    ["should load a standard save (Japan) (Both)"     ,            "both.sav", ['t|["Sonic 3","Sonic 3 & Knuckles","Competition"]$1', 't|["Slot 4"]$2', "c|c7fb", "i|Tails$1"        , "i|0$2", "w|1$2", "c|81c2"]],
   ])("%s", async (...args) =>
     await snippet(`${game}/${args[1]}`, args[2]),
   );
