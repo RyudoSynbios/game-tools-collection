1ff6c14b3962de8816b8a04786e82503d601baf7
Guillaume Molle <heryu@hotmail.fr>
2024-05-27 19:00:10 +0200

    fix: facultative type of littleEndian 24 bits functions on DataView

--- src/app.d.ts
+++ src/app.d.ts
@@ -19,13 +19,13 @@ declare global {
     getBit: (offset: number, bit: Bit) => number;
     getLower4: (offset: number) => number;
     getUpper4: (offset: number) => number;
-    getUint24: (offset: number, littleEndian: boolean) => number;
-    getInt24: (offset: number, littleEndian: boolean) => number;
+    getUint24: (offset: number, littleEndian?: boolean) => number;
+    getInt24: (offset: number, littleEndian?: boolean) => number;
     setBit: (offset: number, bit: Bit, value) => void;
     setLower4: (offset: number, value: number) => void;
     setUpper4: (offset: number, value: number) => void;
-    setUint24: (offset: number, value: number, littleEndian: boolean) => void;
-    setInt24: (offset: number, value: number, littleEndian: boolean) => void;
+    setUint24: (offset: number, value: number, littleEndian?: boolean) => void;
+    setInt24: (offset: number, value: number, littleEndian?: boolean) => void;
   }
 
   interface Number {
