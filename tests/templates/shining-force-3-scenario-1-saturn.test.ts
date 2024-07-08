fbeea4a038be664da4d3a7eed5d16027099b3605
Guillaume Molle <heryu@hotmail.fr>
2024-07-08 08:16:20 +0200

    refactor: add Weapons tab on Shining Force 3: Scenario 1 (SATURN) [save]

--- tests/templates/shining-force-3-scenario-1-saturn.test.ts
+++ tests/templates/shining-force-3-scenario-1-saturn.test.ts
@@ -22,12 +22,12 @@ describe(game, () => {
   // prettier-ignore
   test.each([
     ["should load a deleted standard save (Europe)",       "deleted.bkr", ['t|[]']],
-    ["should load a deleted standard save (Slot 1)", "deleted-slot1.bkr", ['t|["Slot 1"]'         , "s|1$1", "s|2$2", "c|0xd065", "i|PASS"    , "w|QASS"     , "c|0xd066"]],
-    ["should load a deleted standard save (Slot 2)", "deleted-slot2.bkr", ['t|["Slot 2"]'         , "s|2$1", "s|2$2", "c|0xd109", "i|PASS"    , "w|QASS"     , "c|0xd0ca"]],
-    ["should load a standard save (Europe)"        ,        "europe.bkr", ['t|["Slot 1","Slot 2"]', "s|2$1", "s|2$2", "c|0xd109", "i|PASS"    , "w|QASS"     , "c|0xd0ca"]],
-    ["should load a standard save (USA)"           ,           "usa.bkr", ['t|["Slot 1"]'         , "s|1$1", "s|2$2", "c|0xd186", "i|PASS"    , "w|QASS"     , "c|0xd147"]],
-    ["should load a standard save (Japan)"         ,         "japan.bkr", ['t|["Slot 1"]'         , "s|1$1", "s|2$2", "c|0xf1f8", "i|PASS"    , "w|QASS"     , "c|0xf1b9"]],
-    ["should load a standard save (Japan) (Rev A)" ,    "japan-reva.bkr", ['t|["Slot 1"]'         , "s|1$1", "s|2$2", "c|0xf341", "i|こ\"うかく", "w|さ\"うかく", "c|0xf322"]],
+    ["should load a deleted standard save (Slot 1)", "deleted-slot1.bkr", ['t|["Slot 1"]'         , "s|1$1", "s|2$2", "c|0xd065", "i|PASS"    , "w|QASS"     , "c|0xd046"]],
+    ["should load a deleted standard save (Slot 2)", "deleted-slot2.bkr", ['t|["Slot 2"]'         , "s|2$1", "s|2$2", "c|0xd109", "i|PASS"    , "w|QASS"     , "c|0xd0aa"]],
+    ["should load a standard save (Europe)"        ,        "europe.bkr", ['t|["Slot 1","Slot 2"]', "s|2$1", "s|2$2", "c|0xd109", "i|PASS"    , "w|QASS"     , "c|0xd0aa"]],
+    ["should load a standard save (USA)"           ,           "usa.bkr", ['t|["Slot 1"]'         , "s|1$1", "s|2$2", "c|0xd186", "i|PASS"    , "w|QASS"     , "c|0xd127"]],
+    ["should load a standard save (Japan)"         ,         "japan.bkr", ['t|["Slot 1"]'         , "s|1$1", "s|2$2", "c|0xf1f8", "i|PASS"    , "w|QASS"     , "c|0xf199"]],
+    ["should load a standard save (Japan) (Rev A)" ,    "japan-reva.bkr", ['t|["Slot 1"]'         , "s|1$1", "s|2$2", "c|0xf341", "i|こ\"うかく", "w|さ\"うかく", "c|0xf302"]],
   ])("%s", async (...args) =>
     await snippet(`${game}/${args[1]}`, args[2]),
   );
