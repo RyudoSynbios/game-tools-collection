c66475666e211ccace042e25275b830b0a1c0080
Guillaume Molle <heryu@hotmail.fr>
2024-09-20 18:36:39 +0200

    fix: tests on Rogue Legacy (WIN) [save]

--- tests/templates/rogue-legacy-windows.test.ts
+++ tests/templates/rogue-legacy-windows.test.ts
@@ -11,8 +11,8 @@ describe(game, () => {
 
   // prettier-ignore
   test.each([
-    ["should load a standard General save (World)", "RogueLegacyPlayer.rcdat", ["s|1$1", "s|3$2", "i|PASS"]],
-    ["should load a standard Manor save (World)"  ,     "RogueLegacyBP.rcdat", ["s|2$1", "s|1$2", "i|8$1", "i|2$2"]],
+    ["should load a standard Main save (World)" , "RogueLegacyPlayer.rcdat", ["s|3$1", "i|PASS"]],
+    ["should load a standard Manor save (World)",     "RogueLegacyBP.rcdat", ["s|5$1", "s|1$2", "i|8$1", "i|2$2"]],
   ])("%s", async (...args) =>
     await snippet(`${game}/${args[1]}`, args[2]),
   );
