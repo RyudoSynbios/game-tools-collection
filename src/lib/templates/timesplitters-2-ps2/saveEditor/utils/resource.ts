be3ff2c61371294942a3d5e52c568ae2ad9d798d
Guillaume Molle <heryu@hotmail.fr>
2024-12-12 09:08:48 +0100

    refactor: various

--- src/lib/templates/timesplitters-2-ps2/saveEditor/utils/resource.ts
+++ src/lib/templates/timesplitters-2-ps2/saveEditor/utils/resource.ts
@@ -5,45 +5,41 @@ export const arcadeLeagues = [
       {
         name: "Beginners Series",
         matchs: [
-          { index: 0, name: "Adios Amigos!", fields: ["Kills", "Time"] },
-          { index: 1, name: "Casualty", fields: ["Kills", "Time"] },
-          { index: 2, name: "Top Shot", fields: ["Time", "Lives"] },
+          { index: 0x0, name: "Adios Amigos!", fields: ["Kills", "Time"] },
+          { index: 0x1, name: "Casualty", fields: ["Kills", "Time"] },
+          { index: 0x2, name: "Top Shot", fields: ["Time", "Lives"] },
         ],
       },
       {
         name: "Mode Madness",
         matchs: [
-          { index: 4, name: "Chastity Chased", fields: ["Kills", "Time"] },
-          {
-            index: 3,
-            name: "Shrinking from the Cold",
-            fields: ["Kills", "Time"],
-          },
-          { index: 5, name: "Scrap Metal", fields: ["Kills", "Time"] },
+          { index: 0x4, name: "Chastity Chased", fields: ["Kills", "Time"] },
+          { index: 0x3, name: "Shrinking from the Cold", fields: ["Kills", "Time"] },
+          { index: 0x5, name: "Scrap Metal", fields: ["Kills", "Time"] },
         ],
       },
       {
         name: "It's a Blast",
         matchs: [
-          { index: 6, name: "Night Shift", fields: ["Rank", "Kills"] },
-          { index: 7, name: "Spoils of War", fields: ["Score", "Time"] },
-          { index: 8, name: "Demolition Derby", fields: ["Rank", "Kills"] },
+          { index: 0x6, name: "Night Shift", fields: ["Rank", "Kills"] },
+          { index: 0x7, name: "Spoils of War", fields: ["Score", "Time"] },
+          { index: 0x8, name: "Demolition Derby", fields: ["Rank", "Kills"] },
         ],
       },
       {
         name: "Too Hot to Handle",
         matchs: [
-          { index: 9, name: "Monkey Immolation", fields: ["Time"] },
-          { index: 11, name: "Disco Inferno", fields: ["Time"] },
-          { index: 10, name: "Burns Department", fields: ["Time"] },
+          { index: 0x9, name: "Monkey Immolation", fields: ["Time"] },
+          { index: 0xb, name: "Disco Inferno", fields: ["Time"] },
+          { index: 0xa, name: "Burns Department", fields: ["Time"] },
         ],
       },
       {
         name: "Team Series A",
         matchs: [
-          { index: 12, name: "Club Soda", fields: ["Kills", "Time"] },
-          { index: 13, name: "Station Stand", fields: ["Score", "Time"] },
-          { index: 14, name: "Men in Grey", fields: ["Time"] },
+          { index: 0xc, name: "Club Soda", fields: ["Kills", "Time"] },
+          { index: 0xd, name: "Station Stand", fields: ["Score", "Time"] },
+          { index: 0xe, name: "Men in Grey", fields: ["Time"] },
         ],
       },
     ],
@@ -54,45 +50,45 @@ export const arcadeLeagues = [
       {
         name: "Maximus",
         matchs: [
-          { index: 20, name: "Cold Corpse Caper", fields: ["Rank", "Kills"] },
-          { index: 19, name: "Killer Queen", fields: ["Kills", "Time"] },
-          { index: 18, name: "R109 Beta", fields: ["Rank", "Kills"] },
+          { index: 0x14, name: "Cold Corpse Caper", fields: ["Rank", "Kills"] },
+          { index: 0x13, name: "Killer Queen", fields: ["Kills", "Time"] },
+          { index: 0x12, name: "R109 Beta", fields: ["Rank", "Kills"] },
         ],
       },
       {
         name: "Elimination Series",
         matchs: [
           {
-            index: 21,
+            index: 0x15,
             name: "Baking for the Taking",
             fields: ["Time", "Lives"],
           },
-          { index: 22, name: "Brace Yourself", fields: ["Time", "Lives"] },
-          { index: 23, name: "Starship Whoopers", fields: ["Rank", "Kills"] },
+          { index: 0x16, name: "Brace Yourself", fields: ["Time", "Lives"] },
+          { index: 0x17, name: "Starship Whoopers", fields: ["Rank", "Kills"] },
         ],
       },
       {
         name: "Burns 'n' Bangs",
         matchs: [
-          { index: 25, name: "Chinese Burns", fields: ["Kills", "Time"] },
-          { index: 24, name: "Snow Business", fields: ["Kills", "Time"] },
-          { index: 26, name: "Rocket Man", fields: ["Kills", "Time"] },
+          { index: 0x19, name: "Chinese Burns", fields: ["Kills", "Time"] },
+          { index: 0x18, name: "Snow Business", fields: ["Kills", "Time"] },
+          { index: 0x1a, name: "Rocket Man", fields: ["Kills", "Time"] },
         ],
       },
       {
         name: "Outnumbered but Never...",
         matchs: [
-          { index: 27, name: "Someone Has Got to Pay...", fields: ["Kills"] },
-          { index: 28, name: "Time to Split", fields: ["Kills", "Time"] },
-          { index: 29, name: "Can't Handle This", fields: ["Kills", "Time"] },
+          { index: 0x1b, name: "Someone Has Got to Pay...", fields: ["Kills"] },
+          { index: 0x1c, name: "Time to Split", fields: ["Kills", "Time"] },
+          { index: 0x1d, name: "Can't Handle This", fields: ["Kills", "Time"] },
         ],
       },
       {
         name: "Team Series B",
         matchs: [
-          { index: 15, name: "Hack a Hacker", fields: ["Kills", "Time"] },
-          { index: 16, name: "Rice Cracker Rush", fields: ["Bags", "Time"] },
-          { index: 17, name: "Superfly Lady", fields: ["Time"] },
+          { index: 0xf, name: "Hack a Hacker", fields: ["Kills", "Time"] },
+          { index: 0x10, name: "Rice Cracker Rush", fields: ["Bags", "Time"] },
+          { index: 0x11, name: "Superfly Lady", fields: ["Time"] },
         ],
       },
     ],
@@ -103,49 +99,41 @@ export const arcadeLeagues = [
       {
         name: "One Shot Thrills",
         matchs: [
-          { index: 30, name: "Babes in the Woods", fields: ["Rank", "Kills"] },
-          { index: 31, name: "Double Bill", fields: ["Score", "Time"] },
-          { index: 32, name: "Nikki Jinki Bricky", fields: ["Kills", "Time"] },
+          { index: 0x1e, name: "Babes in the Woods", fields: ["Rank", "Kills"] },
+          { index: 0x1f, name: "Double Bill", fields: ["Score", "Time"] },
+          { index: 0x20, name: "Nikki Jinki Bricky", fields: ["Kills", "Time"] },
         ],
       },
       {
         name: "Duel Meaning",
         matchs: [
-          {
-            index: 37,
-            name: "If I'm Ugly - You Smell!",
-            fields: ["Lives", "Time"],
-          },
-          { index: 38, name: "Golem Guru", fields: ["Lives", "Time"] },
-          { index: 36, name: "Golden Thights", fields: ["Kills", "Time"] },
+          { index: 0x25, name: "If I'm Ugly - You Smell!", fields: ["Lives", "Time"] },
+          { index: 0x26, name: "Golem Guru", fields: ["Lives", "Time"] },
+          { index: 0x24, name: "Golden Thights", fields: ["Kills", "Time"] },
         ],
       },
       {
         name: "Frantic Series",
         matchs: [
-          { index: 40, name: "Hangar Hat's Off!", fields: ["Kills", "Time"] },
-          {
-            index: 39,
-            name: "Can't Please Everyone...",
-            fields: ["Score", "Time"],
-          },
-          { index: 41, name: "Big Top Blowout", fields: ["Rank", "Kills"] },
+          { index: 0x28, name: "Hangar Hat's Off!", fields: ["Kills", "Time"] },
+          { index: 0x27, name: "Can't Please Everyone...", fields: ["Score", "Time"] },
+          { index: 0x29, name: "Big Top Blowout", fields: ["Rank", "Kills"] },
         ],
       },
       {
         name: "Team Series C",
         matchs: [
-          { index: 33, name: "Bags of Fun", fields: ["Bags"] },
-          { index: 34, name: "They're Not Pets!", fields: ["Rank", "Kills"] },
-          { index: 35, name: "Nice Threads", fields: ["Time"] },
+          { index: 0x21, name: "Bags of Fun", fields: ["Bags"] },
+          { index: 0x22, name: "They're Not Pets!", fields: ["Rank", "Kills"] },
+          { index: 0x23, name: "Nice Threads", fields: ["Time"] },
         ],
       },
       {
         name: "Sincerest Form of Flattery",
         matchs: [
-          { index: 42, name: "Aztec the Dino Hunter", fields: ["Kills"] },
-          { index: 43, name: "Half Death", fields: ["Kills", "Time"] },
-          { index: 44, name: "Dead Fractioon", fields: ["Kills", "Time"] },
+          { index: 0x2a, name: "Aztec the Dino Hunter", fields: ["Kills"] },
+          { index: 0x2b, name: "Half Death", fields: ["Kills", "Time"] },
+          { index: 0x2c, name: "Dead Fractioon", fields: ["Kills", "Time"] },
         ],
       },
     ],
@@ -156,57 +144,57 @@ export const challengeSets = [
   {
     name: "Glass Smash",
     challenges: [
-      { index: 0, name: "Plane in the Neck", fields: ["Time"] },
-      { index: 1, name: "Bricking it", fields: ["Time"] },
-      { index: 2, name: "Stain Removal", fields: ["Panes", "Time"] },
+      { index: 0x0, name: "Plane in the Neck", fields: ["Time"] },
+      { index: 0x1, name: "Bricking it", fields: ["Time"] },
+      { index: 0x2, name: "Stain Removal", fields: ["Panes", "Time"] },
     ],
   },
   {
     name: "Behead The Undead",
     challenges: [
-      { index: 15, name: "Fight Off The Living Dead", fields: ["Score"] },
-      { index: 17, name: "Sergio's Last Stand", fields: ["Score"] },
-      { index: 16, name: "Day of the Dammed", fields: ["Score"] },
+      { index: 0xf, name: "Fight Off The Living Dead", fields: ["Score"] },
+      { index: 0x11, name: "Sergio's Last Stand", fields: ["Score"] },
+      { index: 0x10, name: "Day of the Dammed", fields: ["Score"] },
     ],
   },
   {
     name: "Infiltration",
     challenges: [
-      { index: 3, name: "Silent but Deadly", fields: ["Score", "Time"] },
-      { index: 4, name: "Trouble at the Docks", fields: ["Score", "Time"] },
-      { index: 5, name: "Escape from NeoTokyo", fields: ["Score", "Time"] },
+      { index: 0x3, name: "Silent but Deadly", fields: ["Score", "Time"] },
+      { index: 0x4, name: "Trouble at the Docks", fields: ["Score", "Time"] },
+      { index: 0x5, name: "Escape from NeoTokyo", fields: ["Score", "Time"] },
     ],
   },
   {
     name: "Banana Chomp",
     challenges: [
-      { index: 6, name: "Gone Bananas", fields: ["Time"] },
-      { index: 7, name: "Monkey Business", fields: ["Time"] },
-      { index: 8, name: "Playing With Fire", fields: ["Bananas"] },
+      { index: 0x6, name: "Gone Bananas", fields: ["Time"] },
+      { index: 0x7, name: "Monkey Business", fields: ["Time"] },
+      { index: 0x8, name: "Playing With Fire", fields: ["Bananas"] },
     ],
   },
   {
     name: "Cut-out Shoot-out",
     challenges: [
-      { index: 9, name: "Take 'em Down", fields: ["Score"] },
-      { index: 10, name: "Fall Out", fields: ["Score"] },
-      { index: 11, name: "Pick Yer Piece", fields: ["Score"] },
+      { index: 0x9, name: "Take 'em Down", fields: ["Score"] },
+      { index: 0xa, name: "Fall Out", fields: ["Score"] },
+      { index: 0xb, name: "Pick Yer Piece", fields: ["Score"] },
     ],
   },
   {
     name: "TimeSplitters 'Story' Classic",
     challenges: [
-      { index: 13, name: "Badass Buspass Impass", fields: ["Time"] },
-      { index: 14, name: "But Where do the Batteries Go?", fields: ["Time"] },
-      { index: 12, name: "Hit Me Baby One Morgue Time", fields: ["Time"] },
+      { index: 0xd, name: "Badass Buspass Impass", fields: ["Time"] },
+      { index: 0xe, name: "But Where do the Batteries Go?", fields: ["Time"] },
+      { index: 0xc, name: "Hit Me Baby One Morgue Time", fields: ["Time"] },
     ],
   },
   {
     name: "Monkeying Around",
     challenges: [
-      { index: 18, name: "Simian Shootout", fields: ["Score"] },
-      { index: 19, name: "Monkey Mayhem", fields: ["Score"] },
-      { index: 20, name: "Dam Bursters", fields: ["Score"] },
+      { index: 0x12, name: "Simian Shootout", fields: ["Score"] },
+      { index: 0x13, name: "Monkey Mayhem", fields: ["Score"] },
+      { index: 0x14, name: "Dam Bursters", fields: ["Score"] },
     ],
   },
 ];
