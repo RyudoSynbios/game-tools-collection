6ebdd788f53df429a9db237109b3f6f74ea019c9
Guillaume Molle <heryu@hotmail.fr>
2025-02-06 22:00:52 +0100

    feat: add Byteswap

---
+++ src/routes/tools/byteswap/+page.svelte
@@ -0,0 +1,126 @@
+<script lang="ts">
+  import FileSaver from "file-saver";
+
+  import { page } from "$app/stores";
+  import { byteswap } from "$lib/utils/bytes";
+
+  let inputEl: HTMLInputElement;
+  let isDragging = false;
+  let fileIsLoading = false;
+
+  function handleDrop(event: DragEvent): void {
+    const files = event.dataTransfer?.files;
+
+    if (files) {
+      const file = files[0];
+
+      handleUploadedFile(file);
+    }
+  }
+
+  function handleDragLeave(): void {
+    isDragging = false;
+  }
+
+  function handleDragOver(): void {
+    isDragging = true;
+  }
+
+  function handlebyteswapClick(): void {
+    if (!fileIsLoading) {
+      inputEl.click();
+    }
+  }
+
+  function handleInputChange(event: Event): void {
+    const files = (event.target as HTMLInputElement).files || [];
+    const file = files[0];
+
+    handleUploadedFile(file);
+  }
+
+  function handleUploadedFile(file: File): void {
+    if (!file || file.size === 0) {
+      return;
+    }
+
+    isDragging = false;
+    fileIsLoading = true;
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = (event: ProgressEvent<FileReader>) => {
+      const dataView = new DataView(event.target?.result as ArrayBufferLike);
+
+      const dataViewByteswapped = byteswap(dataView);
+
+      const data = new Uint8Array(dataViewByteswapped.buffer);
+
+      const blob = new Blob([data], {
+        type: "application/octet-stream",
+      });
+
+      FileSaver.saveAs(blob, file.name);
+
+      fileIsLoading = false;
+    };
+
+    fileReader.readAsArrayBuffer(file);
+  }
+</script>
+
+<svelte:head>
+  <title>Byteswap | Game Tools Collection</title>
+  <meta property="og:title" content="Byteswap" />
+  <meta property="og:image" content="{$page.url.origin}/img/icon.png" />
+</svelte:head>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div class="gtc-byteswap">
+  <div
+    class="gtc-byteswap-inner"
+    on:click={handlebyteswapClick}
+    on:dragleave|preventDefault={handleDragLeave}
+    on:dragover|preventDefault={handleDragOver}
+    on:drop|preventDefault={handleDrop}
+  >
+    <p class="gtc-byteswap-title">Byteswap</p>
+    {#if isDragging}
+      <p>Drop the file here.</p>
+    {:else if !fileIsLoading}
+      <p>Drag 'n' drop here or click to add a file to convert.</p>
+    {:else}
+      <p>Converting...</p>
+    {/if}
+    <p class="gtc-byteswap-hint">
+      This tool will convert the endianness of a binary file.
+    </p>
+    <input type="file" bind:this={inputEl} on:change={handleInputChange} />
+  </div>
+</div>
+
+<style lang="postcss">
+  .gtc-byteswap {
+    @apply m-auto select-none rounded bg-primary-900 p-2;
+
+    width: 600px;
+    height: 400px;
+
+    & .gtc-byteswap-inner {
+      @apply flex h-full w-full cursor-pointer flex-col items-center justify-center border-2 border-dashed border-primary-500 p-4 text-white;
+
+      & .gtc-byteswap-title {
+        @apply mb-4 text-xl;
+      }
+
+      & .gtc-byteswap-hint {
+        @apply whitespace-pre-line text-center text-primary-400;
+      }
+
+      & input {
+        @apply hidden;
+      }
+    }
+  }
+</style>
