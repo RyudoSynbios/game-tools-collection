4b9012bb6f2f87564ebd0a304bc9ccb09c1c471f
Guillaume Molle <heryu@hotmail.fr>
2025-03-19 19:28:32 +0100

    refactor: improve debugging

---
+++ src/routes/+layout.ts
@@ -0,0 +1,20 @@
+import { get } from "svelte/store";
+
+import { debugOptions } from "$lib/stores";
+import { setLocalStorage } from "$lib/utils/format";
+
+const isBrowser = typeof window !== "undefined";
+
+if (isBrowser) {
+  window.debugGTC = {
+    toggleTool: (tool: string) => {
+      const $debugOptions = get(debugOptions);
+
+      $debugOptions[tool] = !$debugOptions[tool];
+
+      debugOptions.set($debugOptions);
+
+      setLocalStorage("debugOptions", `${JSON.stringify($debugOptions)}`);
+    },
+  };
+}
