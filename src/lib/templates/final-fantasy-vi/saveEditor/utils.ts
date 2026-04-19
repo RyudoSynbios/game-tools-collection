9b830643916296958bf1b3949c61caacf1cb2a2e
Guillaume Molle <heryu@hotmail.fr>
2026-04-19 15:56:44 +0200

    feat: add Final Fantasy VI Advance compatibility

--- src/lib/templates/final-fantasy-vi/saveEditor/utils.ts
+++ src/lib/templates/final-fantasy-vi/saveEditor/utils.ts
@@ -1,23 +1,46 @@
 import { get } from "svelte/store";
 
-import { gameTemplate } from "$lib/stores";
-import { getInt, getString, setInt } from "$lib/utils/bytes";
+import { gameRegion, gameTemplate } from "$lib/stores";
+import { bitToOffset, getInt, getString, setInt } from "$lib/utils/bytes";
 import { formatChecksum } from "$lib/utils/checksum";
-import { clone, isInRange } from "$lib/utils/format";
-import { getItem, getShift, updateResources } from "$lib/utils/parser";
+import { getHeaderShift } from "$lib/utils/common/gameBoyAdvance";
+import { clone, getRegionArray, isInRange } from "$lib/utils/format";
+import {
+  getItem,
+  getShift,
+  updateResourceOrder,
+  updateResources,
+  updateResourcesGroups,
+} from "$lib/utils/parser";
 import { checkValidator } from "$lib/utils/validator";
 
 import type {
+  Binary,
   Item,
   ItemBitflags,
   ItemChecksum,
   ItemContainer,
   ItemInt,
+  ItemSection,
   ItemString,
+  ItemTab,
+  ItemTabs,
   Resource,
+  ResourceGroups,
 } from "$lib/types";
 
-import { characterList, locationList } from "./utils/resource";
+import { gbaResources } from "./utils/gba/resource";
+import {
+  IndexedResource,
+  Resources,
+  snesResources,
+} from "./utils/snes/resource";
+
+let platform = "";
+
+export function initHeaderShift(dataView: DataView): number {
+  return getHeaderShift(dataView);
+}
 
 export function overrideGetRegions(
   dataView: DataView,
@@ -27,13 +50,9 @@ export function overrideGetRegions(
 
   const itemContainer = $gameTemplate.items[0] as ItemContainer;
 
-  // Temporary disable GBA compatibility
-  if (isGBASave(dataView)) {
-    return [];
-  }
-
   for (let i = 0x0; i < itemContainer.instances; i += 0x1) {
-    const itemChecksum = clone(itemContainer.items[0] as ItemChecksum);
+    const itemSection = clone(itemContainer.items[0]) as ItemSection;
+    const itemChecksum = clone(itemSection.items[0]) as ItemChecksum;
 
     const tmpShift = shift + i * itemContainer.length;
 
@@ -51,11 +70,262 @@ export function overrideGetRegions(
       checksum !== 0x0 &&
       checksum === getInt(itemChecksum.offset, "uint16", {}, dataView)
     ) {
-      return ["usa", "japan"];
+      if (isGBASave(dataView, shift)) {
+        platform = "gba";
+        return ["europe", "usa", "japan"];
+      } else {
+        platform = "snes";
+        return ["usa", "japan"];
+      }
     }
   }
 
   return [];
+}
+
+export function overrideParseItem(item: Item): Item | ItemTab {
+  const $gameRegion = get(gameRegion);
+
+  if ("id" in item && item.id?.match(/checksumGba-/)) {
+    const itemChecksum = item as ItemChecksum;
+
+    const [slotIndex] = item.id.splitInt();
+
+    if (platform !== "gba") {
+      itemChecksum.offset = 0x0;
+      itemChecksum.disabled = true;
+    }
+
+    itemChecksum.control.offsetStart -= slotIndex * 0x600;
+    itemChecksum.control.offsetEnd -= slotIndex * 0x600;
+
+    return itemChecksum;
+  } else if ("id" in item && item.id === "gbaOnly") {
+    const itemInt = item as ItemInt;
+
+    if (platform !== "gba") {
+      itemInt.offset = 0x0;
+      itemInt.hidden = true;
+    }
+
+    return itemInt;
+  } else if ("id" in item && item.id === "snesOnly") {
+    const itemInt = item as ItemInt;
+
+    itemInt.hidden = platform !== "snes";
+
+    return itemInt;
+  } else if ("id" in item && item.id === "europeOnly") {
+    const itemInt = item as ItemInt;
+
+    if ($gameRegion !== 0) {
+      itemInt.offset = 0x0;
+      itemInt.hidden = true;
+    }
+
+    return itemInt;
+  } else if ("id" in item && item.id?.match(/cross-/)) {
+    const itemInt = item as ItemInt;
+
+    if (platform !== "gba") {
+      return itemInt;
+    }
+
+    const [, type] = item.id.split("-");
+
+    switch (type) {
+      case "currentGroup":
+        itemInt.offset += 0x288;
+        break;
+      case "experience":
+        itemInt.max = 5000000;
+        break;
+      case "vigor":
+        itemInt.name = "Strength";
+        break;
+      case "magicPower":
+        itemInt.name = "Magic";
+        break;
+      case "skills":
+        itemInt.name = "Abilities";
+        break;
+    }
+
+    itemInt.name;
+
+    return itemInt;
+  } else if ("id" in item && item.id === "magic") {
+    const itemTab = item as ItemTab;
+
+    const resources = getResources();
+
+    const itemInt = itemTab.items[0] as ItemInt;
+
+    if (platform === "gba") {
+      itemInt.offset -= 0x205;
+    }
+
+    itemTab.items = [
+      {
+        type: "tabs",
+        items: resources.magic.map((magic) => ({
+          name: magic.name,
+          flex: true,
+          items: magic.spells.map((spell) => ({
+            ...itemInt,
+            id: `spell-${platform}-%index%-${spell.index}`,
+            name: spell.name,
+            offset: itemInt.offset + spell.index,
+          })),
+        })),
+      },
+    ];
+
+    return itemTab;
+  } else if ("id" in item && item.id?.match(/bitflags-/)) {
+    const itemTab = item as ItemTab;
+
+    const [, type] = item.id.split("-") as [undefined, keyof Resources];
+
+    const resources = getResources();
+
+    let resource = resources[type] as IndexedResource[];
+    let offset = (itemTab.items[0] as ItemBitflags).flags[0].offset;
+    let overrideShift: { parent: number; shift: number } | undefined;
+    let flagPerItem = 14;
+
+    if (type === "rareItems") {
+      flagPerItem = 22;
+    } else if (type === "espers" && platform === "gba") {
+      offset += 0x1b97;
+      overrideShift = { parent: -1, shift: 0x400 };
+    } else if (type === "swordTechs" && platform === "gba") {
+      itemTab.name = "Bushido";
+    } else if (type === "enemies" && platform === "gba") {
+      resource = resource.slice(0x0, 0x100);
+    }
+
+    itemTab.items = [];
+
+    const count = Math.floor((resource.length - 1) / flagPerItem);
+
+    for (let i = 0; i <= count; i += 1) {
+      itemTab.items.push({
+        type: "bitflags",
+        overrideShift,
+        flags: resource
+          .slice(i * flagPerItem, i * flagPerItem + flagPerItem)
+          .map((item) => ({
+            offset: offset + bitToOffset(item.index),
+            bit: item.index % 8,
+            label: item.name,
+          })),
+      });
+    }
+
+    return itemTab;
+  } else if ("id" in item && item.id === "inventory") {
+    const itemTabs = item as ItemTabs;
+
+    const resources = getResources();
+
+    const itemTab = itemTabs.items[0] as ItemTab;
+    const itemInt = itemTab.items[0] as ItemInt;
+    const rareItems = itemTabs.items[1] as ItemTab;
+
+    let overrideShift: { parent: number; shift: number } | undefined;
+    let binary: Binary | undefined;
+
+    if (platform === "gba") {
+      itemInt.offset += 0x1dc7;
+      binary = { bitStart: 0, bitLength: 7 };
+      overrideShift = { parent: -1, shift: 0x400 };
+    }
+
+    itemTabs.items = [
+      {
+        name: "Weapons",
+        items: resources.itemTypes
+          .filter((type) => isInRange(type.index, 0x0, 0x9))
+          .map((type) => ({
+            name: type.name,
+            type: "section",
+            flex: true,
+            items: resources.items
+              .filter((item) => item.type === type.index)
+              .map((item) => ({
+                ...itemInt,
+                id: `quantity-${item.index}`,
+                name: item.name,
+                binary,
+                overrideShift,
+              })),
+          })),
+      },
+      ...resources.itemTypes
+        .filter((type) => type.index >= 0xa)
+        .map(
+          (type) =>
+            ({
+              name: type.name,
+              flex: true,
+              items: resources.items
+                .filter((item) => item.type === type.index)
+                .map((item) => ({
+                  ...itemInt,
+                  id: `quantity-${item.index}`,
+                  name: item.name,
+                  binary,
+                  overrideShift,
+                })),
+            }) as ItemTab,
+        ),
+    ];
+
+    itemTabs.items.push(rareItems);
+
+    return itemTabs;
+  } else if ("id" in item && item.id === "bestiary") {
+    const itemTab = item as ItemTab;
+
+    if (platform !== "gba") {
+      return itemTab;
+    }
+
+    const resources = getResources();
+
+    const enemies = resources.enemies
+      .filter((enemy) => enemy.bestiaryIndex !== -1)
+      .sort((a, b) => a.bestiaryIndex! - b.bestiaryIndex!);
+
+    const itemInt = itemTab.items[0] as ItemInt;
+
+    const itemTabs = { type: "tabs", vertical: true, items: [] } as ItemTabs;
+
+    const count = Math.floor((enemies.length - 1) / 20);
+
+    for (let i = 0; i <= count; i += 1) {
+      const start = i * 20;
+      const end = Math.min(start + 20, enemies.length);
+
+      itemTabs.items.push({
+        name: `${`${start + 1}`.padStart(3, "0")}-${`${end}`.padStart(3, "0")}`,
+        flex: true,
+        items: enemies.slice(start, end).map((enemy) => ({
+          ...itemInt,
+          name: `${`${enemy.bestiaryIndex}`.padStart(3, "0")} ${enemy.name}`,
+          offset: itemInt.offset + enemy.index * 0x2,
+        })),
+      });
+    }
+
+    itemTab.items = [itemTabs];
+    itemTab.hidden = false;
+
+    return itemTab;
+  }
+
+  return item;
 }
 
 export function overrideParseContainerItemsShifts(
@@ -74,6 +344,10 @@ export function overrideParseContainerItemsShifts(
   }
 
   return [false, undefined];
+}
+
+export function onReady(): void {
+  generateResources();
 }
 
 export function overrideItem(item: Item): Item {
@@ -154,14 +428,16 @@ export function overrideGetInt(item: Item): [boolean, number | undefined] {
 
     const [itemIndex] = item.id.splitInt();
 
-    const inventory = getInventory(itemInt.offset - 0x100);
+    const inventory = getInventory(itemInt.offset);
 
     const index = inventory.findIndex((index) => index === itemIndex);
 
     let quantity = 0;
 
     if (index !== -1) {
-      quantity = getInt(itemInt.offset + index, "uint8");
+      quantity = getInt(itemInt.offset + index, "uint8", {
+        binary: itemInt.binary,
+      });
     }
 
     return [true, quantity];
@@ -194,7 +470,7 @@ export function overrideSetInt(item: Item, value: string): boolean {
 
     const quantity = parseInt(value);
 
-    updateInventory(itemInt.offset - 0x100, itemIndex, quantity);
+    updateInventory(itemInt.offset, itemIndex, quantity);
 
     return true;
   }
@@ -206,11 +482,13 @@ export function afterSetInt(item: Item): void {
   if ("id" in item && item.id === "location") {
     const itemInt = item as ItemInt;
 
+    const resources = getResources();
+
     const locationIndex = getInt(itemInt.offset, "uint16", {
       binary: itemInt.binary,
     });
 
-    const location = locationList.find(
+    const location = resources.locations.find(
       (location) => location.index === locationIndex,
     );
 
@@ -221,9 +499,11 @@ export function afterSetInt(item: Item): void {
   } else if ("id" in item && item.id === "character") {
     const itemInt = item as ItemInt;
 
+    const resources = getResources();
+
     const characterIndex = getInt(itemInt.offset, "uint8");
 
-    const character = characterList.find(
+    const character = resources.characters.find(
       (character) => character.index === characterIndex,
     );
 
@@ -249,6 +529,10 @@ export function generateChecksum(
 ): number {
   let checksum = 0x0;
 
+  if ("id" in item && item.id?.match(/checksumGba-/)) {
+    checksum = 0x1;
+  }
+
   for (let i = item.control.offsetStart; i < item.control.offsetEnd; i += 0x1) {
     checksum += getInt(i, "uint8", {}, dataView);
   }
@@ -256,14 +540,118 @@ export function generateChecksum(
   return formatChecksum(checksum, item.dataType);
 }
 
-export function isGBASave(dataView?: DataView): boolean {
+export function isGBASave(dataView: DataView, shift: number): boolean {
   const validator = [
     0x46, 0x49, 0x4e, 0x41, 0x4c, 0x20, 0x46, 0x41, 0x4e, 0x54, 0x41, 0x53,
     0x59, 0x20, 0x56, 0x49, 0x20, 0x20, 0x20, 0x20, 0x20, 0x41, 0x44, 0x56,
     0x41, 0x4e, 0x43, 0x45, 0x20, 0x20, 0x20, 0x20,
   ]; // "FINAL FANTASY VI     ADVANCE    "
 
-  return checkValidator(validator, 0x1f00, dataView);
+  return checkValidator(validator, shift + 0x1f00, dataView);
+}
+
+function getResources(): Resources {
+  return platform === "gba" ? gbaResources : snesResources;
+}
+
+function generateResources(): void {
+  const resources = getResources();
+
+  // Abilities
+
+  updateResources("abilities", resources.abilities);
+
+  // Characters
+
+  const characters = resources.characters.reduce(
+    (characters: Resource, character) => {
+      characters[character.index] = character.name;
+
+      return characters;
+    },
+    { 0xff: "-" },
+  );
+
+  updateResources("characters", characters);
+
+  // Config
+
+  updateResources("commandSettings", resources.configCommandSettings);
+  updateResources("cursorPositions", resources.configCursorPositions);
+  updateResources("reequips", resources.configReequips);
+
+  // Espers
+
+  const espers: Resource = { 0xff: "-" };
+  const espersOrder: number[] = [0xff];
+
+  resources.espers.forEach((esper) => {
+    espers[esper.index] = esper.name;
+    espersOrder.push(esper.index);
+  });
+
+  updateResources("espers", espers);
+  updateResourceOrder("espers", espersOrder);
+
+  // Inventory
+
+  const bodies: Resource = { 0xff: "-" };
+  const hands: Resource = { 0xff: "-" };
+  const heads: Resource = { 0xff: "-" };
+  const relics: Resource = { 0xff: "-" };
+
+  const handsGroups: ResourceGroups = [];
+
+  resources.itemTypes
+    .filter((type) => isInRange(type.index, 0x0, 0xa))
+    .forEach((type) => {
+      handsGroups.push({ name: type.name, options: [] });
+    });
+
+  resources.items.forEach((item) => {
+    if (isInRange(item.type, 0x0, 0xa)) {
+      hands[item.index] = item.name;
+      handsGroups[item.type].options.push(item.index);
+    } else if (item.type === 0xb) {
+      heads[item.index] = item.name;
+    } else if (item.type === 0xc) {
+      bodies[item.index] = item.name;
+    } else if (item.type === 0xf) {
+      relics[item.index] = item.name;
+    }
+  });
+
+  updateResources("bodies", bodies);
+  updateResources("hands", hands);
+  updateResources("heads", heads);
+  updateResources("relics", relics);
+  updateResourcesGroups("hands", handsGroups);
+
+  // Letters
+
+  updateResources("letters", getRegionArray(resources.letters));
+
+  // Locations
+
+  const locations: Resource = {};
+  const locationsOrder: number[] = [];
+  const locationsGroups: ResourceGroups = [];
+
+  resources.areas.forEach((area) => {
+    locationsGroups.push({ name: area.name, options: [] });
+  });
+
+  resources.locations
+    .sort((a, b) => a.order - b.order)
+    .forEach((location) => {
+      locations[location.index] = location.name;
+      locationsGroups[location.area].options.push(location.index);
+      locationsOrder.push(location.index);
+    });
+
+  updateResources("locations", locations);
+  updateResourceOrder("locations", locationsOrder);
+  updateResourcesGroups("locations", locationsGroups);
 }
 
 export function getCharacterNames(slotIndex: number): Resource {
@@ -303,11 +691,23 @@ export function updateCharacterNames(slotIndex: number): void {
   updateResources("characterNames", values);
 }
 
+function getInventoryLength(): number {
+  return platform === "gba" ? 0x120 : 0x100;
+}
+
 function getInventory(offset: number): number[] {
   const inventory: number[] = [];
 
-  for (let i = 0x0; i < 0x100; i += 0x1) {
-    const itemIndex = getInt(offset + i, "uint8");
+  const length = getInventoryLength();
+
+  const itemsOffset = offset - length;
+
+  for (let i = 0x0; i < length; i += 0x1) {
+    let itemIndex = getInt(itemsOffset + i, "uint8");
+
+    if (platform === "gba") {
+      itemIndex |= getInt(offset + i, "bit", { bit: 7 }) << 0x8;
+    }
 
     inventory.push(itemIndex);
   }
@@ -321,21 +721,27 @@ function updateInventory(
   value: number,
 ): void {
   const inventory = getInventory(offset);
+  const length = getInventoryLength();
 
   const index = inventory.findIndex((index) => index === itemIndex);
 
+  if (platform === "gba") {
+    value |= (itemIndex & 0x100) >> 0x1;
+    itemIndex &= 0xff;
+  }
+
   if (value && index !== -1) {
-    setInt(offset + 0x100 + index, "uint8", value);
+    setInt(offset + index, "uint8", value);
   } else if (value) {
     const index = inventory.findIndex((index) => index === 0xff);
 
     if (index !== -1) {
-      setInt(offset + index, "uint8", itemIndex);
-      setInt(offset + 0x100 + index, "uint8", value);
+      setInt(offset - length + index, "uint8", itemIndex);
+      setInt(offset + index, "uint8", value);
     }
   } else if (index !== -1) {
-    setInt(offset + index, "uint8", 0xff);
-    setInt(offset + 0x100 + index, "uint8", 0x0);
+    setInt(offset - length + index, "uint8", 0xff);
+    setInt(offset + index, "uint8", 0x0);
   }
 }
 
@@ -344,6 +750,14 @@ function getSpellShift(
   partyIndex: number,
   spellIndex: number,
 ): number {
+  if (platform === "gba") {
+    if (partyIndex < 0xc) {
+      return partyIndex * 0x40;
+    }
+
+    return -1;
+  }
+
   offset = offset - 0x46e - spellIndex + partyIndex * 0x25;
 
   const characterIndex = getInt(offset, "uint8");
--- tests/templates/final-fantasy-vi.test.ts
+++ tests/templates/final-fantasy-vi.test.ts
@@ -17,17 +17,27 @@ test.beforeAll(async ({ browser }) => initPage(browser, `${game}/save-editor`));
 test.beforeEach(async () => ejectFile());
 
 test.describe(game, () => {
-  defaultTests(game, ["super-nintendo"]);
+  defaultTests(game, ["super-nintendo", "game-boy-advance"]);
 
-  test("should not load an empty standard save", async () => {
-    await saveShouldBeRejected(`${game}/empty.sav`);
+  test("should not load an empty SNES standard save", async () => {
+    await saveShouldBeRejected(`${game}/snes-empty.sav`);
+  });
+
+  test("should not load an empty GBA standard save", async () => {
+    await saveShouldBeRejected(`${game}/gba-empty.sav`);
   });
 
   // prettier-ignore
   const tests: Test[] = [
-    ["should load a standard save (USA)"        ,      "usa.sav", ["r|usa"  , 't|["Slot 1","Slot 2"]', "s|2$1", "c|0xd4ba", "s|2$2", "i|PASS"  , "w|QASS"   , "c|0xd4bb"]],
-    ["should load a standard save (USA) (Rev 1)", "usa-rev1.sav", ["r|usa"  , 't|["Slot 2","Slot 3"]', "s|2$1", "c|0xd479", "s|2$2", "i|PASS"  , "w|QASS"   , "c|0xd47a"]],
-    ["should load a standard save (Japan)"      ,    "japan.sav", ["r|japan", 't|["Slot 1","Slot 3"]', "s|3$1", "c|0xd5a7", "s|2$2", "i|ごうかく", "w|ざうかく", "c|0xd5a9"]],
+    ["should load a standard SNES save (USA)"        ,      "snes-usa.sav", ["r|usa"   , 't|["Slot 1","Slot 2"]', "s|2$1", "c|0xd4ba$1", "s|2$2", "i|PASS"  , "w|QASS"   , "c|0xd4bb$1"]],
+    ["should load a standard SNES save (USA) (Rev 1)", "snes-usa-rev1.sav", ["r|usa"   , 't|["Slot 2","Slot 3"]', "s|2$1", "c|0xd479$1", "s|2$2", "i|PASS"  , "w|QASS"   , "c|0xd47a$1"]],
+    ["should load a standard SNES save (Japan)"      ,    "snes-japan.sav", ["r|japan" , 't|["Slot 1","Slot 3"]', "s|3$1", "c|0xd5a7$1", "s|2$2", "i|ごうかく", "w|ざうかく", "c|0xd5a9$1"]],
+    ["should load a standard GBA save (Europe)"      ,    "gba-europe.sav", ["r|europe", 't|["Slot 1","Slot 2"]', "s|2$1", "c|0xaed4$1", "c|0x1f5f$2", "c|0x000e$3", "s|2$2", "i|PASS"  , "w|QASS"   , "s|3$2", "s|8$3", "i|1$2", "w|2$2", "s|5$2", "i|8$1", "w|9$1", "c|0xaed5$1", "c|0x1f60$2", "c|0x000f$3"]],
+    ["should load a standard GBA save (USA)"         ,       "gba-usa.sav", ["r|usa"   , 't|["Slot 1","Slot 3"]', "s|1$1", "c|0xad61$1", "c|0x1f61$2", "c|0x0011$3", "s|2$2", "i|PASS"  , "w|QASS"   , "s|3$2", "s|8$3", "i|3$2", "w|4$2", "s|5$2", "i|8$1", "w|9$1", "c|0xad62$1", "c|0x1f62$2", "c|0x0012$3"]],
+    ["should load a standard GBA save (Japan)"       ,     "gba-japan.sav", ["r|japan" , 't|["Slot 2","Slot 3"]', "s|3$1", "c|0xb151$1", "c|0x1f60$2", "c|0x000e$3", "s|2$2", "i|ごうかく", "w|ざうかく", "s|3$2", "s|8$3", "i|1$2", "w|2$2", "s|5$2", "i|8$1", "w|9$1", "c|0xb153$1", "c|0x1f61$2", "c|0x000f$3"]],
+    ["should load a GameShark GBA save (Europe)"     ,    "gba-europe.sps", ["r|europe", 't|["Slot 1","Slot 2"]', "s|2$1", "c|0xaed4$1", "c|0x1f5f$2", "c|0x000e$3", "s|2$2", "i|PASS"  , "w|QASS"   , "s|3$2", "s|8$3", "i|1$2", "w|2$2", "s|5$2", "i|8$1", "w|9$1", "c|0xaed5$1", "c|0x1f60$2", "c|0x000f$3"]],
+    ["should load a GameShark GBA save (USA)"        ,       "gba-usa.sps", ["r|usa"   , 't|["Slot 1","Slot 3"]', "s|1$1", "c|0xad61$1", "c|0x1f61$2", "c|0x0011$3", "s|2$2", "i|PASS"  , "w|QASS"   , "s|3$2", "s|8$3", "i|3$2", "w|4$2", "s|5$2", "i|8$1", "w|9$1", "c|0xad62$1", "c|0x1f62$2", "c|0x0012$3"]],
+    ["should load a GameShark GBA save (Japan)"      ,     "gba-japan.sps", ["r|japan" , 't|["Slot 2","Slot 3"]', "s|3$1", "c|0xb151$1", "c|0x1f60$2", "c|0x000e$3", "s|2$2", "i|ごうかく", "w|ざうかく", "s|3$2", "s|8$3", "i|1$2", "w|2$2", "s|5$2", "i|8$1", "w|9$1", "c|0xb153$1", "c|0x1f61$2", "c|0x000f$3"]],
   ];
 
   tests.forEach(([title, saveFilePath, args]) => {
